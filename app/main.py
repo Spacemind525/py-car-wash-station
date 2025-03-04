54cc00d9b5e8ba06944659ad4e2397a2610d172b
Alex <zigzaksasha@gmail.com>
2025-03-04 18:11:00 +0100

    god_loves_the_trinity

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars) -> float:
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -27,18 +27,18 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car) -> float:
+    def calculate_washing_price(self, car: list) -> float:
         cost = (
             car.comfort_class * (self.clean_power - car.clean_mark)
             * (self.average_rating / self.distance_from_city_center)
         )
         return round(cost, 1)
 
-    def wash_single_car(self, car) -> None:
+    def wash_single_car(self, car: list) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating) -> None:
+    def rate_service(self, rating: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         total_rating += rating
         self.count_of_ratings += 1
